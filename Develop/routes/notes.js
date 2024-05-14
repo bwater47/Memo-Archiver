5eb9140312916a71fee3b140edb815a01efd35d0
B Water <brennw47@gmail.com>
2024-05-14 15:15:21 -0600

    Updated notes.json to be db.json per say the grading requirements/Updated notes.js to include a title for the newNote that is appended to the database(db.json), and then also removed username, and topic from the if statement, then changed all of the instances of notes.json to db.json/Updated server.js to include middle ware for parsing JSON and urlencoded form data, then added the middleware for the /api endpoint however it is not connecting to any route at the moment will fix, then added middleware for serving the static files, added a get route for the homepage, a get route for the notes page, and a listener for the server so that when npm run start is executed the port can be entered into the url and the server can be viewed.
    Updated notes.json to be db.json per say the grading requirements. Updated notes.js to include a title for the newNote that is appended to the database(db.json), and then also removed username, and topic from the if statement, then changed all of the instances of notes.json to db.json. Updated server.js to include middle ware for parsing JSON and urlencoded form data, then added the middleware for the /api endpoint however it is not connecting to any route at the moment will fix, then added middleware for serving the static files, added a get route for the homepage, a get route for the notes page, and a listener for the server so that when npm run start is executed the port can be entered into the url and the server can be viewed.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -9,13 +9,13 @@ const {
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // GET Route for a specific note
 notes.get('/:note_id', (req, res) => {
   const noteId = req.params.note_id;
-  readFromFile('./db/notes.json')
+  readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       const result = json.filter((note) => note.note_id === noteId);
@@ -28,14 +28,14 @@ notes.get('/:note_id', (req, res) => {
 // DELETE Route for a specific note
 notes.delete('/:note_id', (req, res) => {
   const noteId = req.params.note_id;
-  readFromFile('./db/notes.json')
+  readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all notes except the one with the ID provided in the URL
       const result = json.filter((note) => note.note_id !== noteId);
 
       // Save that array to the filesystem
-      writeToFile('./db/notes.json', result);
+      writeToFile('./db/db.json', result);
 
       // Respond to the DELETE request
       res.json(`Item ${noteId} has been deleted 🗑️`);
@@ -50,13 +50,12 @@ notes.post('/', (req, res) => {
 
   if (req.body) {
     const newNote = {
-      username,
+      title,
       note,
-      topic,
       note_id: uuidv4(),
     };
 
-    readAndAppend(newNote, './db/notes.json');
+    readAndAppend(newNote, './db/db.json');
     res.json(`note added successfully`);
   } else {
     res.error('Error in adding note');
--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,21 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 // Middleware console log function.
 app.use(clog);
+// Middleware for parsing JSON and urlencoded form data.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+// Middleware for serving static files.
+app.use(express.static('public'));
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+// Listener for the server.
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
